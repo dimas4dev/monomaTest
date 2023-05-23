94699cfe4f0a00b49d06167337957e8ba7cefa62
Dimas Mendoza <damendoza98@gmail.com>
2023-05-22 22:58:13 -0500

    add .env file

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: parseInt(process.env.VITE_PORT) || 5270,
+  },
   plugins: [react()],
 })
